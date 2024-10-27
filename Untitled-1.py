c85dc0aa42c7fce2644bd11f0c1d3bf764a4c75e
KajGwo <kajetangwozdz@gmail.com>
2024-10-27 14:52:07 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,36 @@
+#WIP
+#I
+print(int(7+4))
+#II
+print(int(10+40)/2)
+#III
+print(int(2**3))
+#IV
+print(int(7>5))
+#V
+r=4
+#VI
+area=3.14159 * r * r
+#VII
+print(area)
+#VIII
+print(round(area, 2))
+#IX
+first_name = "Mary"
+#X
+last_name = "Brown"
+#XI
+full_name = first_name + ' ' + last_name
+#XII
+full_name
+#XIII
+print(full_name)
+#XIV 
+len(full_name)
+#XV
+a = 5
+h = 4
+print(int((a*h)/2))
+#XVI
+x=((7+12+31)/3)
+print(round(x,2))
