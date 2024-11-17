392810c4750d091b0aee7ab0f12c3f31f21eda6c
KajGwo <kajetangwozdz@gmail.com>
2024-11-17 15:16:10 +0100

    Add files via upload

---
+++ wiek student itp.py
@@ -0,0 +1,42 @@
+import math 
+# A program that calculates and prints:
+# - the number of people and percentage of the totalpopulation living in the Northern Hemisphere
+# - the number of people and percentage of the total population living in the Southern Hemisphere
+ 
+total = 8 
+north = 7.2
+south = 0.8
+print("Northern Hemisphere in %: ", north/total*100)
+
+# A program that calculates and prints
+# the average grade of a student
+math = 5
+art = 4
+music = 5
+history = 3
+average = (math + art + music + history) / 4
+print("Average grade is", average)
+
+#4. Output Formatting
+
+#My name is Adam.
+#I am ... years old, and my height is ... cm.
+#In 6 years I will be ... years old.
+
+name = (input("imię?: "))
+wiek = int(input("wiek?: "))
+wzrost = int(input("wzrost?: "))
+print("My name is", name , "I am", wiek, "years old, and my height is", wzrost, "cm In 6 years I will be", wiek + 6 ,"years old")
+
+
+
+
+
+
+
+
+
+
+
+
+
