24686173bf66cd8608400bb8f4f404c3c4502078
KajGwo <kajetangwozdz@gmail.com>
2024-11-17 15:24:18 +0100

    Add files via upload

---
+++ imiona i populacja.py
@@ -0,0 +1,29 @@
+import math 
+
+fi = 5450
+mi = 4920
+fm = 5
+ti = fi + mi
+ipp = (fi + mi)/5
+print(f'Total family income is {ti}, and income per person is {ipp}')
+
+a = 3
+b = 5
+print(f'{a} + {b} = { a+ b} ')
+print(f'{a} - {b} = {a - b} ')
+print(f'{a} * {b} = {a * b} ')
+print(f'{a} / {b} = {a / b}')
+
+fn = input('Enter your first name: ')
+ln= input('Enter your last name: ')
+
+print(f'Your first name is {fn} and your last name is {ln} and your fullname is {fn +" "+ ln}')
+
+
+
+
+
+
+
+
+
